21e9c635498fc9250161765f679eb4fe51efbe3a
eg1379 <eg587@cam.ac.uk>
2022-05-19 17:48:28 +0100

    previous years

---
+++ hx_examples.py
@@ -0,0 +1,14 @@
+
+
+#2017 A1
+hx = HX(tube_number = 8,
+        baffle_number = 12,
+        pitch = 10e-3,
+        tube_length = 261e-3,
+        shell_length = 334e-3,
+        baffle_gap = 16e-3,
+        baffle_type = 'across',
+        tube_layout='t',
+        shell_passes=1,
+        tube_passes=1,
+        nozzle_bore=19e-3)
--- optimisationroutine.py
+++ optimisationroutine.py
@@ -3,6 +3,8 @@ import hx_functions as hxf
 from hx_classes import HX
 n = 0
 
+
+
 for tube_length in range(0,269):#incorporates length constraint
     for tube_number in range(1,30):
         if tube_length * tube_number <= 3500:
@@ -10,6 +12,17 @@ for tube_length in range(0,269):#incorporates length constraint
             print('solution')
             for baffle_number in range(0,50):
                 for shell_passes in range(1,4):
+                    hx = HX(tube_number = tube_number,
+                            baffle_number = baffle_number,
+                            pitch = 12e-3,
+                            tube_length = tube_length,
+                            shell_length = 450e-3,
+                            baffle_gap  = 13.9e-3,
+                            baffle_type = 'across',
+                            tube_layout='t',
+                            shell_passes = shell_passes,
+                            tube_passes=1,
+                            nozzle_bore=25e-3)
                     if HX.total_mass(HX) <= 1.1:
                         print('fair solution')
                         n+=1
