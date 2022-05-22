3e6b5b4066371dabd2e5965c1b2deaa4ca9cafac
eg1379 <eg587@cam.ac.uk>
2022-05-22 23:17:59 +0100

    ni

--- hx_functions.py
+++ hx_functions.py
@@ -6,6 +6,8 @@ from scipy.interpolate import interp1d
 from matplotlib import pyplot as plt
 from hx_classes import HX, water
 import pandas as pd
+from numpy import pi
+from math import sqrt 
 
 #HYDRAULIC DESIGN
 
@@ -893,3 +895,84 @@ def brute_opt(step = 20):
     with pd.option_context('display.max_rows', None, 'display.max_columns', None,"display.precision", 3):  # more options can be specified also
         print(hx_data)
 
+def brute_opt_2():
+    #brute optimisation but applying some common sense to reduce time
+    baffle_type = 'across_c'
+    tube_layout = 't'
+
+    hx_designs = {}
+    hx_data = pd.DataFrame()
+    K_hot = 1.8
+    K_cold = 1
+    
+    design_no = 0
+
+    pitch = 12e-3
+    crossflow_rows = 4.5
+    tube_bundle_diameter = (crossflow_rows * pitch + 8e-3)/1000
+    plenum_length_1 = 41
+    plenum_length_2 = 41
+
+    for tube_passes in range(1,4):
+        for tube_number in range(10,15):
+            for tube_length in range(100,250,30):
+                if tube_number * tube_length <= 3500:
+                    for shell_passes in range(1,2):
+                        if shell_passes%2 == 0:
+                            bso_min = 10
+                        else:
+                            bso_min = 41
+                        baffle_spacing_in = 41
+                        for baffle_spacing_out in range(bso_min, 90, 40):
+                            for baffle_number in range(10,14):
+                                for baffle_gap in range(10,60,10):
+                                    design_no += 1
+                                    heat_exchanger = HX(tube_number = tube_number,
+                                                        baffle_number = baffle_number,
+                                                        pitch = 12/1000,
+                                                        tube_length = tube_length/1000,
+                                                        plenum_length_1 = 41/1000,
+                                                        plenum_length_2 = 41/1000,
+                                                        baffle_gap = baffle_gap/1000,
+                                                        baffle_type = baffle_type,
+                                                        tube_layout = tube_layout,
+                                                        shell_passes = shell_passes,
+                                                        tube_bundle_diameter  = tube_bundle_diameter,
+                                                        tube_passes = tube_passes,
+                                                        baffle_spacing_in = baffle_spacing_in/1000,
+                                                        baffle_spacing_out = baffle_spacing_out/1000,
+                                                        design_year = 2022,
+                                                        pump_year = 2022,
+                                                        T_inh = 53.4,
+                                                        T_inc = 19.2,
+                                                        leakage = True,
+                                                        name = None,
+                                                        co_counter='counter',
+                                                        approximate_glue_mass=0
+                                                        )
+                                    
+                                    if heat_exchanger.total_mass() <= 1.1:
+                                        hx_designs[f'design {design_no}'] = heat_exchanger
+                                        performance = hx_design(heat_exchanger,K_hot,K_cold)
+                                        hx_data = hx_data.append(performance, ignore_index = True) 
+                                        hx_data.sort_values(by="Q_LMTD (kW)").head()
+
+    #order columns nicely
+    #hx_data = hx_data[['Name',
+    #            'T cold in (C)',
+    #            'T cold out (C)',
+    #            'T hot in (C)',
+    #            'T hot out (C)',
+    #            'mdot_cold (l/s)',
+    #            'mdot_hot (l/s)',
+    #            'dP_cold (bar)',
+    #            'dP_hot (bar)',
+    #            'Q_LMTD (kW)',
+    #            'eff_LMTD',
+    #            'Q_NTU (kW)',
+    #            'eff_NTU',
+    #            'mass (kg)'
+    #                ]]
+    hx_data = hx_data[['Name', 'Q_LMTD (kW)', 'eff_LMTD', 'Q_NTU (kW)', 'eff_NTU']]
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None,"display.precision", 3):  # more options can be specified also
+        print(hx_data)
