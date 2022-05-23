ed70821a728a4a8580b3ca524a4ed2bb05f90bb8
Tom Tyler <tbt24@cam.ac.uk>
2022-05-23 02:46:40 +0100

    fdf

--- hx_functions.py
+++ hx_functions.py
@@ -7,7 +7,8 @@ from matplotlib import pyplot as plt
 from hx_classes import HX, water, pipe
 import pandas as pd
 from numpy import pi
-from math import sqrt 
+from math import sqrt
+import openpyxl
 
 #HYDRAULIC DESIGN
 
@@ -951,7 +952,7 @@ def brute_opt(n = 10,K_hot = 1.8,K_cold = 1):
                 #     bso_min = 41e-3
                 bso_array = np.array([41e-3]) #np.linspace(bso_min,50e-3,n)
                 for tube_number in tn_array:
-                    print('tube_number',tube_number)
+                    #print('tube_number',tube_number)
 
                     for plenum_length_1 in pl1_array:                        
 
@@ -1012,7 +1013,7 @@ def brute_opt(n = 10,K_hot = 1.8,K_cold = 1):
                                                             
 
     #order columns nicely
-    hx_data = hx_data.sort_values(by="Q_NTU (kW)", ascending=False).head(100)[['Name',
+    hx_data = hx_data.sort_values(by="Q_NTU (kW)", ascending=False).head(1000)[['Name',
                                                                                 'Q_NTU (kW)',
                                                                                 'eff_NTU',
                                                                                 'mass (kg)',
