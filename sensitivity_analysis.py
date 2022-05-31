d891e869729dff581e2412f5da91d3c09a3cdbda
eg1379 <eg587@cam.ac.uk>
2022-05-31 15:09:07 +0100

    enfwei

--- sensitivity_analysis.py
+++ sensitivity_analysis.py
@@ -5,14 +5,18 @@ from hx_classes import HX
 performance = []
 tn = []
 
-for tube_number in range(15,40):
-    hx =  HX(tube_number = tube_number,
+k_array = [5.09582324e-13, 1.06322974e-02, 1.60725045e+01, 7.70041192e-14,
+9.74222768e-01, 7.39759976e+00, 6.78054364e+01, 7.84344345e-01,
+3.25600821e-01, 8.46334898e-01]
+
+for n in range(1,200):
+    hx =  HX(tube_number = 20,
              baffle_number = 6,
              pitch = 10e-3,
              tube_length = 172e-3,
              plenum_length_1 = 58e-3,
              plenum_length_2 = 23e-3,
-             baffle_gap = 20e-3,
+             baffle_gap = n/10000,
              baffle_type = 'across_c',
              tube_layout='t',
              shell_passes = 2,
@@ -27,9 +31,8 @@ for tube_number in range(15,40):
              name = '2022_F',
              real_data = None)
 
-    k_array = fit_data()
-    tn.append(tube_number)
-    performance.append(hx_design(hx)['Q_NTU (kW)'])
+    tn.append(n)
+    performance.append(hx_design(hx,k_array)['Q_NTU (kW)'])
 
 plt.plot(tn,performance)
 plt.show()
